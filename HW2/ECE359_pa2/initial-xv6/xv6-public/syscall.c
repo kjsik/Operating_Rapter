d9a5327539f02ab07914e057127ab1a868cb0871
kjsik <127501452+kjsik@users.noreply.github.com>
2025-11-16 22:02:47 +0900

    Add new system call functions for tickets and process info

--- HW2/ECE359_pa2/initial-xv6/xv6-public/syscall.c
+++ HW2/ECE359_pa2/initial-xv6/xv6-public/syscall.c
@@ -105,7 +105,8 @@ extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_setuid(void);
 extern int sys_getreadcount(void);
-
+extern int sys_settickets(void);
+extern int sys_getpinfo(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -130,7 +131,10 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_setuid]     sys_setuid,
+//addition part
 [SYS_getreadcount]  sys_getreadcount,
+[SYS_settickets]  sys_settickets,
+[SYS_getpinfo]  sys_getpinfo,
 };
 
 void
