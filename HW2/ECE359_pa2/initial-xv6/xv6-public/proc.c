563c75a64eff30a27e428d2f571b2555bcd4a01b
kjsik <127501452+kjsik@users.noreply.github.com>
2025-11-17 15:57:20 +0900

    Refactor scheduler to use round-robin algorithm
    Comment out the original lottery scheduling code and replace it with a round-robin scheduling implementation.

--- HW2/ECE359_pa2/initial-xv6/xv6-public/proc.c
+++ HW2/ECE359_pa2/initial-xv6/xv6-public/proc.c
@@ -402,94 +402,94 @@ scheduler(void)
   struct cpu *c = mycpu();
   c->proc = 0;
 
-  uint total_tickets, acc, r, winner;
+  // uint total_tickets, acc, r, winner;
   
-  static int seeded = 0;
-  if(!seeded){
-    srand(ticks);
-    seeded = 1;
-  }
+  // static int seeded = 0;
+  // if(!seeded){
+  //   srand(ticks);
+  //   seeded = 1;
+  // }
 
-  for(;;){
-    sti();
-
-    acquire(&ptable.lock);
-
-    total_tickets = 0;
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
-      total_tickets += p->tickets;
-    }
-
-    if(total_tickets == 0){
-      release(&ptable.lock);
-      continue;
-    }
-    
-    r = rand();
-    winner = (r % total_tickets) + 1;
-
-    acc = 0;
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
-
-      acc += p->tickets;
-
-      if(acc >= winner){
-        c->proc = p;
-        switchuvm(p);
-        p->state = RUNNING;
-        
-        int start_ticks = ticks;
-        swtch(&(c->scheduler), p->context);
-        int end_ticks = ticks;
-        p->ticks += (end_ticks - start_ticks);
-
-        switchkvm();
-
-        c->proc = 0;
-        break;
-      }
-      else{
-        continue;
-      }
-    }
-    release(&ptable.lock);
-  }
-  //RR
   // for(;;){
-  //   // Enable interrupts on this processor.
   //   sti();
 
-  //   // Loop over process table looking for process to run.
   //   acquire(&ptable.lock);
+
+  //   total_tickets = 0;
+  //   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+  //     if(p->state != RUNNABLE)
+  //       continue;
+  //     total_tickets += p->tickets;
+  //   }
+
+  //   if(total_tickets == 0){
+  //     release(&ptable.lock);
+  //     continue;
+  //   }
+    
+  //   r = rand();
+  //   winner = (r % total_tickets) + 1;
+
+  //   acc = 0;
   //   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
   //     if(p->state != RUNNABLE)
   //       continue;
 
-  //     // Switch to chosen process.  It is the process's job
-  //     // to release ptable.lock and then reacquire it
-  //     // before jumping back to us.
-  //     c->proc = p;
-  //     switchuvm(p);
-  //     p->state = RUNNING;
+  //     acc += p->tickets;
 
-  //     const int tickstarts = ticks;
+  //     if(acc >= winner){
+  //       c->proc = p;
+  //       switchuvm(p);
+  //       p->state = RUNNING;
+        
+  //       int start_ticks = ticks;
+  //       swtch(&(c->scheduler), p->context);
+  //       int end_ticks = ticks;
+  //       p->ticks += (end_ticks - start_ticks);
 
-  //     swtch(&(c->scheduler), p->context);
-  //     switchkvm();
+  //       switchkvm();
 
-  //     p->ticks += ticks - tickstarts;
-
-  //     // Process is done running for now.
-  //     // It should have changed its p->state before coming back.
-  //     c->proc = 0;
+  //       c->proc = 0;
+  //       break;
+  //     }
+  //     else{
+  //       continue;
+  //     }
   //   }
   //   release(&ptable.lock);
-
   // }
+  // RR
+  for(;;){
+    // Enable interrupts on this processor.
+    sti();
+
+    // Loop over process table looking for process to run.
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
+        continue;
+
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      c->proc = p;
+      switchuvm(p);
+      p->state = RUNNING;
+
+      const int tickstarts = ticks;
+
+      swtch(&(c->scheduler), p->context);
+      switchkvm();
+
+      p->ticks += ticks - tickstarts;
+
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;
+    }
+    release(&ptable.lock);
+
+  }
 }
 
 // Enter scheduler.  Must hold only ptable.lock
